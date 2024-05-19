f6423c1ab56619e474cf010756c855177eb29bcf
mfurqanjaved <mfurqanjaved@gmail.com>
2024-05-19 05:05:04 +0500

    added data ingestion committed

--- src/logger.py
+++ src/logger.py
@@ -2,8 +2,7 @@ import logging
 import os
 from datetime import datetime
 
-
-LOG_FILE =(f"logs/{datetime.now().strftime('%Y-%m-%d')}.log")
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
 
@@ -11,5 +10,8 @@ LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO)
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
+)
